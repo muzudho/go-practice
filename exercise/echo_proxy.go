c8b501ef25649a4a4814c613c35a951594f119db
muzudho <muzudho1@gmail.com>
2025-11-30 00:40:31 +0900

    echo_proxy

---
+++ exercise/echo_proxy.go
@@ -0,0 +1,95 @@
+package exercise
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+// EchoProxy - 外部プロセスの標準入出力をプロキシする練習
+func EchoProxy(externalProcessPath string) {
+	// // コマンドライン引数登録
+	// exePath := flag.String("exe", "", "Working directory path.")
+	// // 解析
+	// flag.Parse()
+
+	// if *exePath == "" {
+	// 	panic(fmt.Errorf("--exe <Executable file path>"))
+	// }
+
+	// exePath := "C:/Users/むずでょ/go/src/github.com/muzudho/go-echo-next-char/go-echo-next-char.exe"
+	parameters := strings.Split("", " ")
+
+	//externalProcess := exec.Command(*exePath, parameters...) // 外部プロセスコマンド作成
+	externalProcess := exec.Command(externalProcessPath, parameters...) // 外部プロセスコマンド作成
+
+	stdin, err := externalProcess.StdinPipe() // 外部プロセス標準入力パイプ取得
+	if err != nil {
+		panic(err)
+	}
+	defer stdin.Close() // stdin を使い終わったら、外部プロセス標準入力パイプクローズ
+
+	stdout, err := externalProcess.StdoutPipe() // 外部プロセス標準出力パイプ取得
+	if err != nil {
+		panic(err)
+	}
+	defer stdout.Close() // stdout を使い終わったら、外部プロセス標準出力パイプクローズ
+
+	err = externalProcess.Start() // 外部プロセス起動
+	if err != nil {
+		panic(fmt.Errorf("cmd.Start() --> [%s]", err))
+	}
+
+	go receiveStdout(stdout) // 外部プロセスの標準出力受信開始
+
+	// Go言語では標準出力のUTF-8に対応していますが、VSCodeのターミナルはUTF-8に対応していないようです。
+	// `chcp 65001`
+	// そのため、外部プロセスの標準出力を受信しても、正しく表示されない場合があります。
+	// その場合は、WindowsのコマンドプロンプトやPowerShellなど、UTF-8に対応したターミナルで実行してください。
+
+	go receiveStdin(stdin) // 外部プロセスの標準入力送信開始
+
+	externalProcess.Wait()
+}
+
+// receiveStdin - 標準入力受信
+// `epStdin` - External process stdin
+func receiveStdin(epStdin io.WriteCloser) {
+	scanner := bufio.NewScanner(os.Stdin) // 標準入力を読み取るスキャナ作成
+	for scanner.Scan() {
+		command := scanner.Text() // １行読み取り。UTF-8文字列。
+		epStdin.Write([]byte(command))
+		epStdin.Write([]byte("\n"))
+	}
+}
+
+// receiveStdout - 標準出力受信
+// `epStdout` - External process stdout
+func receiveStdout(epStdout io.ReadCloser) {
+	var buffer [1]byte // これが満たされるまで待つ。1バイト。
+
+	p := buffer[:]
+
+	for {
+		n, err := epStdout.Read(p)
+
+		if nil != err {
+			if fmt.Sprintf("%s", err) == "EOF" {
+				// End of file
+				return
+			}
+
+			panic(err)
+		}
+
+		if 0 < n {
+			bytes := p[:n]
+
+			// 思考エンジンから１文字送られてくるたび、表示。
+			print(string(bytes))
+		}
+	}
+}
--- main.go
+++ main.go
@@ -4,26 +4,38 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/muzudho/go-practice/exercise"
 )
 
 func main() {
-	fmt.Print("📁exerciseフォルダ下の📄ファイル名が練習名です。練習名を入力してください　｜　例 strings　｜　例 exit　：")
+	// 📁exerciseフォルダ下の📄ファイル名が練習名です。中には引数が必要なものもあります。練習名を入力してください　｜　例 strings　｜　例 exit　：
+	// を英語で：
+	fmt.Print("Practice name is the file name under the 📁exercise folder. Please enter the practice name ｜ e.g. strings ｜ e.g. exit ：")
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() { // 標準入力を読込みます
-		practiceName := scanner.Text() // 1行ずつテキストを取得します
+		commandLine := scanner.Text() // 1行ずつテキストを取得します
 
-		if practiceName == "exit" {
+		if commandLine == "exit" {
 			break // "exit"と入力されたらループを抜けます
 		}
+
+		// practiceName を空白でスプリットし、最初の要素を取得します
+		tokens := strings.Split(commandLine, " ")
+		practiceName := tokens[0]
 
 		fmt.Printf("練習名：%s\n", practiceName)
 
 		switch practiceName {
 		case "echo_stdio":
 			exercise.EchoStdio()
+		case "echo_proxy":
+			// ```
+			// echo_proxy Z:/muzudho-github.com/muzudho/go-practice/go-practice.exe
+			// ```
+			exercise.EchoProxy(tokens[1])
 		case "fmt":
 			exercise.Fmt()
 		case "strings":

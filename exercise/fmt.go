0e66e6762115c3c73f869719b85cb1a52f0a9883
muzudho <muzudho1@gmail.com>
2025-11-29 23:35:06 +0900

    fmt

--- exercise/fmt.go
+++ exercise/fmt.go
@@ -8,6 +8,62 @@ import (
 
 // Fmt - 文字列出力の練習
 func Fmt() {
+
+	var s = "Hello, world!"
+	fmt.Println(s)
+
+	// 先頭の数文字を読むには（＾ｑ＾）？
+	fmt.Println(s[0])   // なんか１文字取ったら数が出てくるぜ（＾ｑ＾）
+	fmt.Println(s[0:1]) // こう書くと文字が出てくるぜ（＾ｑ＾）
+	fmt.Println(s[0:2])
+	fmt.Println(s[:2])
+	fmt.Println(s[2:])
+
+	// フォーマットも使ってみようぜ（＾ｑ＾）？
+	fmt.Printf("%d\n", s[0]) // 数
+	fmt.Printf("%c\n", s[0]) // %s ではなく %c
+
+	var s2 = "日本語だったらどうなる（＾ｑ＾）？"
+	fmt.Println(s2)
+
+	// 先頭の数文字を読むには（＾ｑ＾）？
+	fmt.Println(s2[0])   // なんか１文字取ったら数が出てくるぜ（＾ｑ＾）
+	fmt.Println(s2[0:1]) // 文字化け
+	fmt.Println(s2[0:2]) // 2bytes 文字化け
+	fmt.Println(s2[0:3]) // 日
+	fmt.Println(s2[0:4]) // 日と1byte文字化け。文字ごとのバイトサイズは分からんなあ
+	fmt.Println(s2[:2])
+	fmt.Println(s2[2:])
+
+	// フォーマットも使ってみようぜ（＾ｑ＾）？
+	fmt.Printf("%d\n", s2[0])
+	fmt.Printf("%c\n", s2[0]) // %s ではなく %c。でもダメ文字化け
+
+	// runeの配列にしろとのこと
+	r3 := []rune(s2)
+
+	fmt.Println("日本語を読みたいぜ（＾ｑ＾）！")
+	// 先頭の数文字を読むには（＾ｑ＾）？
+	fmt.Println(r3[0])         // 26085
+	fmt.Println(string(r3[0])) // 日
+	fmt.Println(string(r3[0:1]))
+	fmt.Println(string(r3[0:2]))
+	fmt.Println(string(r3[:2]))
+	fmt.Println(string(r3[2:]))
+
+	// フォーマットも使ってみようぜ（＾ｑ＾）？
+	fmt.Printf("%d\n", r3[0])           // 26085
+	fmt.Printf("%c\n", r3[0])           // 日
+	fmt.Printf("%c\n", r3[0:3])         // [日 本 語]
+	fmt.Printf("%s\n", string(r3[0:3])) // 日本語
+
+	fmt.Println("runeの配列を回したらいいのかだぜ（＾ｑ＾）！？")
+	// rangeで取り出すとrune単位で取り出せる。
+	for _, c3 := range r3 {
+		fmt.Println(string(c3))
+	}
+	fmt.Println("他の例も見てみるぜ（＾ｑ＾）！")
+
 	// runeの配列にしろとのこと
 	r1 := []rune("ハローワールドだぜ（＾ｑ＾）")
 
@@ -29,9 +85,9 @@ func Fmt() {
 	fmt.Printf("0:2=%s\n", string(r2[0:res2])) // apple
 
 	// 読み取った文字が、アルファベットか、数か 区別する方法あんの（＾ｑ＾）？
-	r3 := []rune("体重は90kgぐらいかだぜ（＾ｑ＾）？")
-	res3 := unicode.IsLetter(r3[3])
-	fmt.Printf("%c is letter? = %t\n", r3[3], res3) // boolean は %t
-	res3 = unicode.IsNumber(r3[3])
-	fmt.Printf("%c is number? = %t\n", r3[3], res3)
+	r4 := []rune("体重は90kgぐらいかだぜ（＾ｑ＾）？")
+	res3 := unicode.IsLetter(r4[3])
+	fmt.Printf("%c is letter? = %t\n", r4[3], res3) // boolean は %t
+	res3 = unicode.IsNumber(r4[3])
+	fmt.Printf("%c is number? = %t\n", r4[3], res3)
 }

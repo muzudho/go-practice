a24f3ed832dcb51945b561d10c5b01b6eb888864
muzudho <muzudho1@gmail.com>
2025-12-02 08:23:13 +0900

    コメント

--- bin/build.bat
+++ bin/build.bat
@@ -3,7 +3,7 @@
 rem 文字化け対策。コマンドプロンプトがデフォルトで Shift-JIS なので、その文字コードを消すことで、UTF-8 にする。
 chcp 65001 >nul
 
-echo `C:/MuzudhoWorks/go-practice` に `go-practice.exe` ファイルを作るぜ（＾～＾）...
+echo 外部ドライブにexeファイル置いても実行権限無いし、`C:/MuzudhoWorks/go-practice` に `go-practice.exe` ファイルを作るぜ（＾～＾）...
 cd ..
 go build -o C:/MuzudhoWorks/go-practice
 cd ./bin
